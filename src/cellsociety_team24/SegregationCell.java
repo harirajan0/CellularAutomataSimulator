4762c16e86a4e1fdb617ac7a7b0148af699ca423
Bihan Zhuang <bihan.zhuang@duke.edu>
2017-02-01 00:08:35 -0500

    made two small changes in the Cell super class: constructor & List; wrote working SegregationCell class

--- src/cellsociety_team24/SegregationCell.java
+++ src/cellsociety_team24/SegregationCell.java
@@ -1,12 +1,79 @@
 package cellsociety_team24;
 
+import java.util.List;
+import java.util.Random;
+
 public class SegregationCell extends Cell {
 	
-	private double satisfactoryLevel = 0.3;
-
-	@Override
-	public void update() {
-		
+	private double percentage;
+	
+	public SegregationCell(String initState, int x, int y, double percentage) {
+		super(initState, x, y);
+		this.percentage = percentage;
 	}
 
+	@Override
+	public void update() {}
+
+	/**
+	 * Updates the state of the Cell if it is dissatisfied and changes 
+	 * a random Cell from the specified ArrayList of empty Cells to the 
+	 * state of the original Cell (state 1 or state 2).
+	 * @param emptyPositions
+	 */
+	public void update(List<Cell> emptyPositions) {
+		if (this.getCurrentState().equals("empty")) return;
+		List<Cell> neighbors = this.getNeighbors();
+		if (!isSatisfied(neighbors)){
+			changeStates(emptyPositions);
+		} else{
+			setNextState(this.getCurrentState());
+		}
+	}
+	
+	/**
+	 * Checks if the Cell is satisfied by comparing the percentage of 
+	 * neighbors being the same type calculate, to the threshold percentage 
+	 * specified by the user.
+	 * @param neighbors
+	 * @return
+	 */
+	private boolean isSatisfied(List<Cell> neighbors){
+		double nonEmpty = 0, sameType = 0;
+		for (Cell nb : neighbors){
+			if (nb != null){
+				if (!nb.getCurrentState().equals("empty")){
+					nonEmpty++;
+				}
+				if (nb.getCurrentState().equals(this.getCurrentState())){
+					sameType++;
+				}
+			}
+		}
+		if (nonEmpty == 0) return true;
+		return (sameType/nonEmpty) >= percentage;
+	}
+	
+	/**
+	 * Changes the NextState of the Cell to empty and randomly selects
+	 * an empty Cell and sets its NextState to this Cell's CurrentState.
+	 * @param emptyPositions
+	 */
+	private void changeStates(List<Cell> emptyPositions){
+		this.setNextState("empty");
+		
+		//System.out.println("1. " + this.getCurrentState());
+		//System.out.println("2. " + this.getNextState());
+		
+		int option = new Random().nextInt(emptyPositions.size());
+		Cell tmp = emptyPositions.get(option);
+		tmp.setNextState(this.getCurrentState());
+		
+		//System.out.println("3. " + tmp.getCurrentState());
+		//System.out.println("4. " + tmp.getNextState());
+		
+		emptyPositions.add(this);
+		emptyPositions.remove(tmp);
+	}
+	
 }
--- src/cellsociety_team24/SpreadingFireCell.java
+++ src/cellsociety_team24/SpreadingFireCell.java
@@ -2,6 +2,11 @@ package cellsociety_team24;
 
 public class SpreadingFireCell extends Cell {
 
+	public SpreadingFireCell(String initState, int x, int y) {
+		super(initState, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
