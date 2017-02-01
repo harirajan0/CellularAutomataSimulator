c5ac33749af73272aee2726fc8bbd1e04c735f2c
Vishnu Gottiparthy <vtg4@duke.edu>
2017-02-01 01:41:20 -0500

    Basic FireCell functionality

--- src/cellsociety_team24/SpreadingFireCell.java
+++ src/cellsociety_team24/SpreadingFireCell.java
@@ -1,15 +1,58 @@
 package cellsociety_team24;
+import java.util.ArrayList;
 
 public class SpreadingFireCell extends Cell 
 {	
-	public SpreadingFireCell(double x, double y, String state)
+	private static final String EMPTY = "empty";
+	private static final String BURNING = "burning";
+	private static final String TREE = "tree";
+	private double probCatch;
+	
+	public SpreadingFireCell(double x, double y, String state, double prob)
 	{
 		super();
+		setxPosition(x);
+		setyPosition(y);
+		setCurrentState(state);
+		probCatch = prob;
+		//setNeighbors(???);
 	}
 	
 	@Override
 	public void update() 
 	{
-		
+		if(isState(EMPTY))
+		{
+			setNextState(EMPTY);
+		}
+		else if(isState(TREE))
+		{
+			setTreeState();
+		}
+		else if(isState(BURNING))
+		{
+			setNextState(EMPTY);
+		}
+	}
+	
+	private boolean isState(String state)
+	{
+		return getCurrentState().equals(state);
+	}
+	
+	private void setTreeState()
+	{
+		ArrayList<Cell> neighbors = getNeighbors();
+		for(Cell cell : neighbors)
+		{
+			if(((SpreadingFireCell)cell).isState(BURNING))
+			{
+				if(Math.random() <= probCatch)
+				{
+					setNextState(BURNING);
+					return;
+				}
+			}
+		}
 	}
 }
