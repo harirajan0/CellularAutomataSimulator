bda50c4773d2eb43fdf6af30a3acc9e1404d3b47
Gabriel <gc104@duke.edu>
2017-02-02 15:22:59 -0500

    Opened Branch GabrielChen to work on Conway Cell

--- src/cellsociety_team24/ConwayCell.java
+++ src/cellsociety_team24/ConwayCell.java
@@ -2,6 +2,7 @@ package cellsociety_team24;
 
 public class ConwayCell extends Cell {
 
+	//begin working on ConwayCell 02/02/2017
 	
 	public ConwayCell(String initState, int x, int y) {
 		super(initState, x, y);
