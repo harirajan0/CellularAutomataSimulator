487166edd9d0e0416b70ce1235a9cc2a04c2bee8
Hari <hari.rajan@duke.edu>
2017-02-03 13:42:34 -0500

    started working on loader

--- src/cellsociety_team24/Loader.java
+++ src/cellsociety_team24/Loader.java
@@ -1,14 +1,30 @@
 package cellsociety_team24;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Loader {
 	
-	public Loader() {
+	public Loader(File dataFile) {
 		
 	}
-
-	public ArrayList<Cell> loadXMl() {
+	public ArrayList<String> loadXMl() {
 		return null;
+	}
+	
+	public String getSimulationType(){
+		return null;
+	}
+	
+	public int getRows(){
+		return 0;
+	}
+	
+	public int getCols(){
+		return 0;
+	}
+	
+	public double getParameter(){
+		return 0;
 	}
 }
