6317a7776645f33cd8c8309797e49f952aa57fb7
Bihan Zhuang <bihan.zhuang@duke.edu>
2017-02-12 19:07:34 -0500

    triangle dimension fixed

--- src/cellshapeviews/TriangleShapeView.java
+++ src/cellshapeviews/TriangleShapeView.java
@@ -1,9 +1,12 @@
 package cellshapeviews;
 
 public class TriangleShapeView extends PolygonShapeView {
+	
+	private double sideLength;
 
 	public TriangleShapeView(int r, int c, double size) {
 		super(r, c, size);
+		this.sideLength = size*1.9;
 		definePolygon();
 	}
 
@@ -21,15 +24,15 @@ public class TriangleShapeView extends PolygonShapeView {
 		coord[0] = x;
 		coord[1] = y;
 		if (sameParity()){
-			coord[2] = x + getSideLength();
+			coord[2] = x + sideLength;
 			coord[3] = y;
-			coord[4] = x + getSideLength()/2;
-			coord[5] = y + Math.sqrt(3.0)/2 * getSideLength();
+			coord[4] = x + sideLength/2;
+			coord[5] = y + Math.sqrt(3.0)/2 * sideLength;
 		} else{
-			coord[2] = x + getSideLength()/2;
-			coord[3] = y + Math.sqrt(3.0)/2 * getSideLength();
-			coord[4] = x - getSideLength()/2;
-			coord[5] = y + Math.sqrt(3.0)/2 * getSideLength();
+			coord[2] = x + sideLength/2;
+			coord[3] = y + Math.sqrt(3.0)/2 * sideLength;
+			coord[4] = x - sideLength/2;
+			coord[5] = y + Math.sqrt(3.0)/2 * sideLength;
 		}
 		return coord;
 	}
@@ -39,18 +42,18 @@ public class TriangleShapeView extends PolygonShapeView {
 		Double[] myXY = new Double[2];
 		if (oddRow()){
 			if (getCol()%2!=0){
-				myXY[0] = getSideLength() * ((getCol()-1)/2 + 0.5);
+				myXY[0] = sideLength * ((getCol()-1)/2 + 0.5);
 			} else{
-				myXY[0] = getSideLength() * (getCol()/2 + 0.5);
+				myXY[0] = sideLength * (getCol()/2 + 0.5);
 			}
 		} else{
 			if (getCol()%2==0){
-				myXY[0] = getSideLength() * getCol()/2;
+				myXY[0] = sideLength * getCol()/2;
 			} else{
-				myXY[0] = getSideLength() * (getCol()+1)/2;
+				myXY[0] = sideLength * (getCol()+1)/2;
 			}	
 		}
-		myXY[1] = getSideLength() * Math.sqrt(3.0)/2 * getRow();
+		myXY[1] = sideLength * Math.sqrt(3.0)/2 * getRow();
 		return myXY;
 	}
 
